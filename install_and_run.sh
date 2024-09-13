0b8fa83165cbdd46e2e1bccf7c2ba35ea320b17d
SamanGhn <141281012+SamanGhn@users.noreply.github.com>
2024-09-13 13:51:16 +0330

    Delete install_and_run.sh

--- install_and_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# آدرس فایل اسکریپت در GitHub
-SCRIPT_URL="https://raw.githubusercontent.com/SamanGhn/Backhaul-Scripts/main/setup_backhaul.sh"
-
-# دانلود و اجرای فایل اسکریپت
-bash <(curl -Ls "$SCRIPT_URL")
