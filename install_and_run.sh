0e1877fd49b72a96eb89f641660ab65221cbb715
SamanGhn <141281012+SamanGhn@users.noreply.github.com>
2024-09-13 13:52:02 +0330

    New File

---
+++ install_and_run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# آدرس فایل اسکریپت در GitHub
+SCRIPT_URL="https://raw.githubusercontent.com/SamanGhn/Backhaul-Scripts/main/setup_backhaul.sh"
+
+# دانلود و اجرای فایل اسکریپت
+bash <(curl -Ls "$SCRIPT_URL")
