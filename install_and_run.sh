034f34af80e73e2619e6f1fdb1d438dd530e616b
SamanGhn <141281012+SamanGhn@users.noreply.github.com>
2024-09-13 12:52:06 +0330

    Update install_and_run.sh

--- install_and_run.sh
+++ install_and_run.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-# تنظیمات ابتدایی
-set -e  # متوقف کردن اجرای اسکریپت در صورت بروز خطا
+# آدرس فایل اسکریپت در GitHub
+SCRIPT_URL="https://raw.githubusercontent.com/SamanGhn/Backhaul-Scripts/main/setup_backhaul.sh"
 
-# آدرس مخزن GitHub
-REPO_URL="https://github.com/SamanGhn/Backhaul-Scripts/raw/main/setup_backhaul.sh"
-SCRIPT_NAME="setup_backhaul.sh"
-
-# دانلود فایل اسکریپت از GitHub
-echo "دانلود فایل اسکریپت از GitHub..."
-curl -L "$REPO_URL" -o "$SCRIPT_NAME"
-
-# تغییر مجوز فایل برای اجرا
-chmod +x "$SCRIPT_NAME"
-
-# اجرای اسکریپت
-echo "اجرای فایل اسکریپت..."
-./"$SCRIPT_NAME"
+# دانلود و اجرای فایل اسکریپت
+bash <(curl -Ls "$SCRIPT_URL")
